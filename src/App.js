e7f4aff37a36106e8a8393701cc4c029fffde48a
Chris Sanchez <c.sanch7@gmail.com>
2020-09-22 17:37:17 -0700

    initial commit, clean up boilerplate & install dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Link to="/">Poké Center</Link>
+        <Link to="/wildencounter">Route 102</Link>
+        <Switch>
+          {/* <Route exact path="/" component={Landing} />
+          <Route exact path="/wildencounter" component={WildEncounter} />
+          <Route exact path="/pokemon/:id" component={Pokemon} /> */}
+        </Switch>
+      </>
+    </Router>
   );
 }
 
